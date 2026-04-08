282adb0ac20e9b552d96bf9f53cc71e15a7c3372
Surya9908virat <swamireddysurya@gmail.com>
2026-04-08 17:34:43 +0530

    fix error in baseurl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://rental-management-backend-zwzi.onrender.com";
+const BASE_URL = "https://rental-management-backend-zwzi.onrender.com/api";
 
 const API = axios.create({
   baseURL: BASE_URL,
